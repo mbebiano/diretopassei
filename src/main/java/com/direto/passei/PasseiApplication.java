78ce9c4cfd38b93c56004bc74f1c965460cf0dcb
MattheusBebiano <mattheus.bebiano@engenharia.ufjf.br>
2022-10-09 16:10:21 -0300

    feat: first commit, add initial structure

---
+++ src/main/java/com/direto/passei/PasseiApplication.java
@@ -0,0 +1,13 @@
+package com.direto.passei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PasseiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PasseiApplication.class, args);
+	}
+
+}
